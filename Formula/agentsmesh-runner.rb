f4834cbc12064bb1dcae4a6aeff03e9342bc5174
yishuiliunian <yishuiliunian@gmail.com>
2026-01-11 23:20:58 +0800

    fix: Use correct v0.1.0 asset names (agentmesh-runner prefix)

--- Formula/agentsmesh-runner.rb
+++ Formula/agentsmesh-runner.rb
@@ -8,17 +8,17 @@ class AgentsmeshRunner < Formula
   license "MIT"
 
   on_macos do
-    url "https://github.com/AgentsMesh/AgentsMeshRunner/releases/download/v0.1.0/agentsmesh-runner_0.1.0_darwin_all.tar.gz"
+    url "https://github.com/AgentsMesh/AgentsMeshRunner/releases/download/v0.1.0/agentmesh-runner_0.1.0_darwin_all.tar.gz"
     sha256 "2ea73b07c42b0eb10c043f4d10d0dbefa2710669acc7b5468f49b779d3db898c"
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/AgentsMesh/AgentsMeshRunner/releases/download/v0.1.0/agentsmesh-runner_0.1.0_linux_amd64.tar.gz"
+      url "https://github.com/AgentsMesh/AgentsMeshRunner/releases/download/v0.1.0/agentmesh-runner_0.1.0_linux_amd64.tar.gz"
       sha256 "7fcbafa0c2a098198719eb594a93d0471613fbc625751bd41413a083ace7eebb"
     end
     on_arm do
-      url "https://github.com/AgentsMesh/AgentsMeshRunner/releases/download/v0.1.0/agentsmesh-runner_0.1.0_linux_arm64.tar.gz"
+      url "https://github.com/AgentsMesh/AgentsMeshRunner/releases/download/v0.1.0/agentmesh-runner_0.1.0_linux_arm64.tar.gz"
       sha256 "42daa491142d16b2e97642becd9a5c5008db15c230b13ac702f87a00d402e758"
     end
   end
